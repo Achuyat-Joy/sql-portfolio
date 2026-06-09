96bee386ae10e9429247d0378f68b82b99b75d80
Achuyat Saha Joy <47113699+Achuyat-Joy@users.noreply.github.com>
2026-06-09 10:26:42 +0600

    Add LeetCode #1633: Percentage of Users Attended a Contest

---
+++ LeetCode/1633_Percentage_of_Users_Attended_a_Contest.sql
@@ -0,0 +1,6 @@
+-- Topic: Basic Aggregate Functions
+
+SELECT contest_id, ROUND(COUNT(contest_id)*100.0/(SELECT COUNT(user_id) FROM Users),2) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage desc, contest_id;
