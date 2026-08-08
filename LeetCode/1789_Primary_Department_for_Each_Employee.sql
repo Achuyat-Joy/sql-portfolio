7916a36e2f445ac571c131e7011110444fae4caf
Achuyat Saha Joy <47113699+Achuyat-Joy@users.noreply.github.com>
2026-08-08 22:37:35 +0600

    Add LeetCode #1789: Primary Department for Each

---
+++ LeetCode/1789_Primary_Department_for_Each_Employee.sql
@@ -0,0 +1,14 @@
+with single_dept as (
+    select employee_id, department_id
+    from Employee 
+    group by employee_id
+    having count(employee_id)=1
+)
+select employee_id, department_id
+from single_dept
+
+union
+
+select employee_id, department_id
+from Employee
+where primary_flag='Y';
